dd593944c4d9d3f0ef64b1b55643e1efd31cb436
Pranshu Shakya <pranshushakya12b20@gmail.com>
2024-08-10 12:17:11 +0530

    Card Removal

---
+++ difficulty_1000to1400/REMOVECARDS.java
@@ -0,0 +1,32 @@
+// https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/REMOVECARDS
+
+import java.util.*;
+
+class RemoveCards {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+            Arrays.sort(arr);
+            int count = 1, max = 0;
+            for (int i = 0; i < n-1; i++) {
+                if (arr[i] == arr[i+1]) {
+                    count++;
+                } else {
+                    if(max < count) {
+                        max = count;
+                    }
+                    count = 1;
+                }
+            }
+            if (count > max) {
+				max = count ;
+			}
+            System.out.println(n - max);
+        }
+    }
+}
